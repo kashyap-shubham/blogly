9d74ade792a85404ad999f7012d30517a784962a
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-06-23 08:04:19 +0530

    refactor db.js file to use class based singleton approach

--- server/config/db.js
+++ server/config/db.js
@@ -2,16 +2,71 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const dbConnect = () => {
-   mongoose.connect(process.env.DB_URL)
-   .then(() => {
-    console.log("Db Connected Successfully");
-   })
-   .catch((error) => {
-    console.log(error.message);
-    console.log("Db Connection Failed! Closing the Server");
-    process.exit(1);
-   })
+// export const dbConnect = () => {
+//    mongoose.connect(process.env.DB_URL)
+//    .then(() => {
+//     console.log("Db Connected Successfully");
+//    })
+//    .catch((error) => {
+//     console.log(error.message);
+//     console.log("Db Connection Failed! Closing the Server");
+//     process.exit(1);
+//    })
+// }
+
+class Database {
+   #connectionString;
+   #isConnected = false;
+
+   constructor(connectionString = process.env.DB_URL) {
+      if (!connectionString) {
+         throw new Error("Database Conection String is required");
+      }
+      this.#connectionString = connectionString;
+   }
+   
+   async connect() {
+      if (!this.#isConnected) {
+         console.log("Already connected to the database");
+         return;
+      }
+
+      try {
+         await mongoose.connect(this.#connectionString);
+         this.#isConnected = true;
+         console.log("Database Connected Successfully");
+
+      } catch(error) {
+         console.error("Databse connection failed: ", error.message);
+         process.exit(1);
+      }
+   }
+
+   async disconnect() {
+      if (!this.#isConnected) {
+         console.log("Database is not Connected");
+         return;
+      }
+
+      try {
+         await mongoose.disconnect();
+         this.#isConnected = false;
+         console.log("Database Disconnected Successfully");
+
+      } catch(error) {
+         console.log("Error during disconnect: ", error.message);
+      }
+   }
+
+   isConnected() {
+      return this.#isConnected;
+   }
+   
+   getConnectionString() {
+      return this.#connectionString;
+   }
+
 }
 
 
+export const database = new Database();

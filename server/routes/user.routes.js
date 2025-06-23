dc718e5d3ca2406dcaf0f60877d64ea216542ff5
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-06-23 09:24:28 +0530

    added logout route to the user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { signIn, signUp } from "../controllers/user.Controller";
+import { logout, signIn, signUp } from "../controllers/user.Controller";
 
 const userRouter = Router();
 
 
 userRouter.post("/signup", signUp);
 userRouter.post("/signin", signIn);
+userRouter.get("/logout", logout);
 
 
 

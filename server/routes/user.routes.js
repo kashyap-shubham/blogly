5fc49c4b5c7d489287157088df1fc70e27283f13
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-06-23 15:39:12 +0530

    crated blogRouter and user proper userRouter, and imported in server.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { logout, signIn, signUp } from "../controllers/user.Controller";
 
-const userRouter = Router();
+export const userRouter = Router();
 
 
 userRouter.route("/signup").post(signUp);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 import express from  "express";
 import { database } from "./config/db.js";
+import { userRouter } from "./routes/user.routes.js";
+import { blogRouter } from "./routes/blog.routes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.use("/api/v1/user", userRoutes);
-app.use("/ap1/v1/admin", createBlog);
-app.use("/ap1/v1/blogs", readBlog);
+app.use("/api/v1/user", userRouter);
+app.use("/ap1/v1/blogs", blogRouter);
 
 
 

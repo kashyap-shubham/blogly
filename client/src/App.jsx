66ba2d5f230b5ff3a48cd317eff43dc6fc5e9d5c
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-06-26 22:17:27 +0530

    intialized react+tailwind for frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-300 text-2xl text-center'>This is empty react + tailwindcss project</h1>
+    </>
+  )
+}
+
+export default App
